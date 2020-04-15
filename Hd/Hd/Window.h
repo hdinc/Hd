64ea94027ff624d5c761dac71186fb601c2b980a
hdinc <macellan091@gmail.com>
2020-04-16 02:13:02 +0300

    first commit

---
+++ Hd/Hd/Window.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+namespace Hd {
+
+class Window {
+
+public:
+    Window(const char* name, int width, int height);
+    ~Window();
+    GLFWwindow* WindowId() { return m_WindowId; }
+    void FullScreen(bool f);
+    void VSync(bool v);
+    void SwapBuffers();
+    void PollEvents();
+    bool ShouldClose();
+
+private:
+    GLFWwindow* m_WindowId;
+    bool m_Vsync = true;
+};
+
+
+inline bool Window::ShouldClose()
+{
+    return glfwWindowShouldClose(m_WindowId);
+}
+
+inline void Window::SwapBuffers()
+{
+    glfwSwapBuffers(m_WindowId);
+}
+
+inline void Window::PollEvents()
+{
+    glfwPollEvents();
+}
+
+}
