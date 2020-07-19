63eee4e27285ad0828b87e25bbc5ae7d1b18fa4f
hdinc <macellan091@gmail.com>
2020-07-19 09:38:20 +0300

    balls

--- include/Hd/Shader.h
+++ include/Hd/Shader.h
@@ -7,21 +7,19 @@ namespace Hd {
 class Shader {
 
 public:
-    GLuint m_ProgramId;
-
     Shader(const char* vertex, const char* fragment);
     Shader(const char* vertex, const char* geometry, const char* fragment);
     Shader(const char* compute);
     ~Shader();
+
     void Bind();
+    GLuint Id() { return m_ProgramId; }
 
 private:
     void CheckError(GLuint id, GLenum pname);
+    GLuint m_ProgramId;
 };
 
-inline void Shader::Bind()
-{
-    glUseProgram(m_ProgramId);
-}
+inline void Shader::Bind() { glUseProgram(m_ProgramId); }
 
 }
