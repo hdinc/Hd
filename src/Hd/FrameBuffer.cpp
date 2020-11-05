68cfe81bcc5fbd57461a63cc7f4e892b418fa6e4
hdinc <macellan091@gmail.com>
2020-11-05 23:52:38 +0300

    hotfix

--- src/Hd/FrameBuffer.cpp
+++ src/Hd/FrameBuffer.cpp
@@ -3,10 +3,12 @@
 namespace Hd {
 
 FrameBuffer::FrameBuffer(Texture& tex)
+    : m_Texture(tex)
 {
-    glGenFramebuffers(1, &m_FrameBufferID);
-    glBindFramebuffer(GL_FRAMEBUFFER, m_FrameBufferID);
+    glGenFramebuffers(1, &m_FrameBufferId);
+    glBindFramebuffer(GL_FRAMEBUFFER, m_FrameBufferId);
     glFramebufferTexture2D(
-        GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex.ID(), 0);
+        GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex.Id(), 0);
 }
+
 }
