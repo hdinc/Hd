a8d7016449fb922184a6dc1ec8333117cc26d745
hdinc <macellan091@gmail.com>
2020-11-05 23:52:38 +0300

    hotfix

--- include/Hd/FrameBuffer.h
+++ include/Hd/FrameBuffer.h
@@ -2,6 +2,7 @@
 #include <Hd/Texture.h>
 
 namespace Hd {
+
 class FrameBuffer {
 public:
     FrameBuffer(Hd::Texture& tex);
@@ -11,13 +12,15 @@ public:
 
 private:
     GLuint m_FrameBufferId;
-    Texture& texture;
+    Texture& m_Texture;
 };
 
 inline void FrameBuffer::Bind()
 {
     glBindFramebuffer(GL_FRAMEBUFFER, m_FrameBufferId);
-    glViewport(0, 0, texture.GetWidth(), texture.GetHeight());
+    glViewport(0, 0, m_Texture.GetWidth(), m_Texture.GetHeight());
 }
 
 inline GLuint FrameBuffer::Id() { return m_FrameBufferId; }
+
+}
