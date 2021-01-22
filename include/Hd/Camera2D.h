88bb65ff18ae5fbb0fc382b258a09c6d7379b794
hdinc <macellan091@gmail.com>
2021-01-22 05:31:41 +0300

    added setupinput function to camera class

--- include/Hd/Camera2D.h
+++ include/Hd/Camera2D.h
@@ -2,6 +2,8 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <Hd/Window.h>
+#include <imgui.h>
 
 namespace Hd {
 
@@ -14,8 +16,9 @@ public:
     void lookAt(glm::vec2 l, float zoom);
     void move(glm::vec2 d);
     void zoom(float zoom);
-    static void onScroll(Camera2D* c, double dx, double dy);
-    static void onFramebufferSizeChange(Camera2D* c, int x, int y);
+
+    template <Camera2D& c, bool hasImgui>
+    static void setupInput();
 
     glm::mat4* getView();
     glm::mat4* getProjection();
@@ -30,8 +33,25 @@ private:
     glm::vec2 mLoc;
     glm::vec2 mBorder;
     float mZoom;
+    bool mDrag = false;
+    glm::vec2 mDragStart;
+
+    bool isinputSUp = false;
+    int callbacks[4] = { 0 };
 
     void updateData();
+
+    template <Camera2D& c, bool hasImgui>
+    static void onScroll(double dx, double dy);
+
+    template <Camera2D& c, bool hasImgui>
+    static void onFramebufferSizeChange(int x, int y);
+
+    template <Camera2D& c, bool hasImgui>
+    static void onMouseMovement(double x, double y);
+
+    template <Camera2D& c, bool hasImgui>
+    static void onMouseButtonClick(int button, int action, int mods);
 };
 
 inline void Camera2D::zoom(float zoom)
@@ -57,7 +77,7 @@ inline void Camera2D::updateData()
 {
     mView = glm::mat4(1);
     mView = glm::scale(mView, glm::vec3(mZoom, mZoom, 1));
-    mView = glm::translate(mView, glm::vec3(mLoc, 0));
+    mView = glm::translate(mView, glm::vec3(-mLoc, 0));
     mVP = mProjection * mView;
 }
 
@@ -66,5 +86,95 @@ inline glm::mat4* Camera2D::getProjection() { return &mProjection; }
 inline glm::mat4* Camera2D::getVP() { return &mVP; }
 inline glm::vec2 Camera2D::getLoc() { return mLoc; }
 inline float Camera2D::getZoom() { return mZoom; }
+
+template <Camera2D& c, bool hasImgui>
+void Camera2D::onScroll(double dx, double dy)
+{
+    (void)dx;
+    if (hasImgui) {
+        if (ImGui::IsAnyWindowHovered())
+            return;
+    }
+
+    if (c.mDrag)
+        return;
+
+    float z = c.getZoom();
+    z *= (dy > 0) ? 1.1 : 1 / 1.1;
+    c.zoom(z);
+}
+
+template <Camera2D& c, bool hasImgui>
+void Camera2D::onFramebufferSizeChange(int x, int y)
+{
+    auto s = (x > y) ? glm::vec2((float)y / x, 1) : glm::vec2(1, (float)x / y);
+    c.mProjection = glm::scale(glm::mat4(1), glm::vec3(s, 1));
+    c.mVP = c.mProjection * c.mView;
+}
+
+template <Camera2D& c, bool hasImgui>
+void Camera2D::onMouseMovement(double x, double y)
+{
+    (void)x;
+    (void)y;
+
+    if (hasImgui) {
+        if (ImGui::IsAnyWindowHovered())
+            return;
+    }
+    auto& w = Hd::Window::getInstance();
+    if (c.mDrag) {
+        auto a = w.getMousePos() - c.mDragStart;
+        a.y /= w.getFramebufferSize().x / w.getFramebufferSize().y;
+        a /= c.mZoom;
+        c.mView = glm::scale(glm::mat4(1), glm::vec3(c.mZoom));
+        c.mView = glm::translate(c.mView, glm::vec3(a - c.mLoc, 0.0));
+    }
+    c.mVP = c.mProjection * c.mView;
+}
+
+template <Camera2D& c, bool hasImgui>
+void Camera2D::onMouseButtonClick(int button, int action, int mods)
+{
+    if (hasImgui) {
+        if (ImGui::IsAnyWindowHovered())
+            return;
+    }
+
+    (void)mods;
+
+    auto& w = Hd::Window::getInstance();
+
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS && c.mDrag == false) {
+        c.mDrag = true;
+        c.mDragStart = w.getMousePos();
+    }
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE && c.mDrag == true) {
+        c.mDrag = false;
+
+        auto a = w.getMousePos() - c.mDragStart;
+        a.y /= w.getFramebufferSize().x / w.getFramebufferSize().y;
+        a /= c.mZoom;
+        c.mLoc -= a;
+    }
+    c.updateData();
+}
+
+template <Camera2D& c, bool hasImgui>
+void Camera2D::setupInput()
+{
+    c.isinputSUp = true;
+    auto& w = Hd::Window::getInstance();
+
+    auto f1 = Camera2D::onScroll<c, hasImgui>;
+    auto f2 = Camera2D::onFramebufferSizeChange<c, hasImgui>;
+    auto f3 = Camera2D::onMouseMovement<c, hasImgui>;
+    auto f4 = Camera2D::onMouseButtonClick<c, hasImgui>;
+
+    c.callbacks[0] = w.ScrollCb.addFunc(f1);
+    c.callbacks[1] = w.FramebufferSizeCb.addFunc(f2);
+    c.callbacks[2] = w.CursorPosCb.addFunc(f3);
+    c.callbacks[3] = w.MouseButtonCb.addFunc(f4);
+}
 
 }
--- src/Hd/Camera2D.cpp
+++ src/Hd/Camera2D.cpp
@@ -13,21 +13,14 @@ Camera2D::Camera2D()
 
 Camera2D::~Camera2D()
 {
-}
+    if (isinputSUp) {
 
-void Camera2D::onScroll(Camera2D* c, double dx, double dy)
-{
-    (void)dx;
-    float z = c->getZoom();
-    z *= (dy > 0) ? 1.1 : 1 / 1.1;
-    c->zoom(z);
-}
-
-void Camera2D::onFramebufferSizeChange(Camera2D* c, int x, int y)
-{
-    auto s = (x > y) ? glm::vec2((float)y / x, 1) : glm::vec2(1, (float)x / y);
-    c->mProjection = glm::scale(glm::mat4(1), glm::vec3(s, 1));
-    c->mVP = c->mProjection * c->mView;
+        auto& w = Hd::Window::getInstance();
+        w.ScrollCb.removeFunc(callbacks[0]);
+        w.FramebufferSizeCb.removeFunc(callbacks[1]);
+        w.CursorPosCb.removeFunc(callbacks[2]);
+        w.MouseButtonCb.removeFunc(callbacks[3]);
+    }
 }
 
 }
