9f31fbd95bc5a9f6b4d22af9cd53f06f049f0bd0
hdinc <macellan091@gmail.com>
2022-09-01 23:03:59 +0300

    fix resource header

--- include/Hd/Resource.h
+++ include/Hd/Resource.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <elfio/elfio.hpp>
 #include <myar.h>
 
@@ -13,10 +15,10 @@ public:
     const char* get_file(std::string filename);
     long get_file_size(std::string filename);
 
-private:
     Resource(const char* elf_file);
     ~Resource();
 
+private:
     ELFIO::elfio elf_reader;
     myar::reader myar_reader;
 
