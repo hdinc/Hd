d4e912b4a62bf58102989b17e3167a1760bf0ec0
hdinc <macellan091@gmail.com>
2023-08-24 14:11:14 +0300

    remove apps

--- apps/balls/res/balls.frag
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-out vec4 c;
-uniform vec4 color;
-
-void main()
-{
-    c=vec4(1,1,1,1);
-}
--- apps/res/shaders/frag.glsl
+++
@@ -1,9 +0,0 @@
-#version 450 core
-
-layout(location = 3) uniform vec4 u_color = vec4(1);
-out vec4 c;
-
-void main()
-{
-    c = u_color;
-}
